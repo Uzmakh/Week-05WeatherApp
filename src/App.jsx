2a30ddb3bf97be5ba2ad152a63cefc36272f264b
Uzma Khan <urbazoha@gmail.com>
2024-05-01 11:32:10 +0500

    Added footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,10 @@ function App() {
 
   return (
     <>
-    
+    <h1>Weather App</h1>
+    <footer>
+    <p>open-source code on <a href="https://github.com/Uzmakh/sheCodesReact-Weather-App">GitHub</a> by Uzma Khan and hosted on <a href="https://ajax-searchengine.netlify.app/">Netlify</a></p>
+    </footer>
     </>
   )
 }
