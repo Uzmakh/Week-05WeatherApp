180b8e898ac7e4add31a9a1e1341da3ef28cf650
Uzma Khan <urbazoha@gmail.com>
2024-05-03 18:55:46 +0500

    Added Bootstrap
    Added Bootstrap in my React-Weather-App through npm

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,15 @@ function App() {
 
   return (
     <>
+    <div className="container">
     <h1>Weather App</h1>
     <footer>
-    <p>open-source code on <a href="https://github.com/Uzmakh/sheCodesReact-Weather-App">GitHub</a> by Uzma Khan and hosted on <a href="https://ajax-searchengine.netlify.app/">Netlify</a></p>
+    <p>
+      open-source code on <a href="https://github.com/Uzmakh/sheCodesReact-Weather-App">GitHub</a> by Uzma Khan and hosted on <a href="https://ajax-searchengine.netlify.app/">Netlify</a>
+    </p>
     </footer>
+    <button className='btn btn-primary'>Hello</button>
+    </div>
     </>
   )
 }
