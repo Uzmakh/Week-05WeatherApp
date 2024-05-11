bfad47ef4efe33151c217a1e78a87745d5cd614b
Uzma Khan <urbazoha@gmail.com>
2024-05-11 22:08:51 +0500

    Update index.jsx

--- src/FormattedDate/index.jsx
+++ src/FormattedDate/index.jsx
@@ -1,27 +1,30 @@
-import React, {useState} from 'react'
+import React from "react";
 
 function FormattedDate(props) {
-  const [weatherData, setWeatherData] = useState(false);
-    console.log(props.date)
-    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  // console.log(props.date);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
-    let day = days[props.date.getDay()];
-    let hours = props.date.getHours();
-    if(hours < 10){
-        hours= `0${hours}`
-    }
-    let minutes = props.date.getMinutes();
-    if(minutes < 10){
-        minutes = `0${minutes}`
-    }
-  return 
-  (
-      <div>
-        {day} {hours}:{minutes}
-    </div>
-        
-  )
-
+  let day = days[props.date.getDay()];
+  let hours = props.date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = props.date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return;
+  <div>
+    {day} {hours}:{minutes}
+  </div>;
 }
 
-export default FormattedDate
+export default FormattedDate;
