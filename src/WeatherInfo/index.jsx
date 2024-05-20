93e3244ee559eb4b669ed3853102c7113ebae6e6
Uzma Khan <urbazoha@gmail.com>
2024-05-20 12:45:38 +0500

    set the icon, temperature and unit stylings

--- src/WeatherInfo/index.jsx
+++ src/WeatherInfo/index.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import FormattedDate from "../FormattedDate"
 import WeatherTemperature from "../WeatherTemperature"
+import './style.css'
 
 function WeatherInfo(props) {
   return (
@@ -17,9 +18,7 @@ function WeatherInfo(props) {
         <div className="col-6">
           <img src={props.data.iconUrl} alt={props.data.description} />
 
-          <WeatherTemperature celsius={props.data.temperature}/>
-
-        
+          <WeatherTemperature celsius={props.data.temperature} />
         </div>
         <div className="col-6">
           <ul>
