1c880bbe77d15dad494e1ef0383118c04cd010a6
Uzma Khan <urbazoha@gmail.com>
2024-05-26 11:21:53 +0500

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <Weather defaultCity="New York"/>
     <footer>
     <p>
-      open-source code on <a href="https://github.com/Uzmakh/sheCodesReact-Weather-App">GitHub</a> by Uzma Khan and hosted on <a href="https://ajax-searchengine.netlify.app/">Netlify</a>
+      open-source code on <a href="https://github.com/Uzmakh/sheCodesReact-Weather-App">GitHub</a> by Uzma Khan and hosted on <a href=" https://react-weather-app-youzees.netlify.app/" >Netlify</a>
     </p>
     </footer>
  
